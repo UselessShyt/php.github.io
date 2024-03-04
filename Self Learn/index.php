b30a94e9150c270e7f98ed5892f31790fe20eb69
UselessShyt <yowinchen2@gmail.com>
2024-03-04 17:59:55 +0800

    update

--- Self Learn/index.php
+++ Self Learn/index.php
@@ -1,8 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" name="username" id=""><br>
+        <label for="password">Password: </label>
+        <input type="password" name="password" id=""><br>
+        <input type="submit" value="Login">
+    </form>    
+</body>
+</html>
+
 <?php
-    $name = "Bro";
-    $food = "Pizza";
-    $price = 22.5;
-    echo "Hello, $name<br>";
-    echo "You like $food<br>";
-    echo "The price will be \$$price"
+    echo $_POST["username"] . "<br>";
+    echo $_POST["password"];
 ?>
